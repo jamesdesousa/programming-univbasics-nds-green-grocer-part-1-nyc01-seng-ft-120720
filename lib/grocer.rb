8f9470a736826cf92afeb4fc9ef35c2921107f14
James DeSousa <james.desousa45@gmail.com>
2020-12-06 23:35:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,7 +1,8 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  count = 0 
+  while collection[name].length 
+  
 
 end
 
