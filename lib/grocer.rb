176029b8a0e5f1488a7ff020797e8d671c106911
James DeSousa <james.desousa45@gmail.com>
2020-12-06 23:40:59 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,7 @@ def find_item_by_name_in_collection(name, collection)
   count = 0 
   while count < collection.length do 
     if collection[count][:item] == name 
-      return collection[num]
+      return collection[count]
     end 
     count = count + 1 
   end 
